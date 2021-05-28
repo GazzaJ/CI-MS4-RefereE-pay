d3d425885dabd741aab87a2eefaa2e87cfbc53c3
GazzaJ <johnge71@gmail.com>
2021-05-28 13:11:41 +0000

    Customise admin tables

--- matches/admin.py
+++ matches/admin.py
@@ -2,9 +2,54 @@ from django.contrib import admin
 from .models import Age_Group, Club, Venue, Team, Game, Fee
 
 
+class ClubAdmin(admin.ModelAdmin):
+    list_display = (
+        'club_name',
+        'club_badge',
+        'website_url',
+    )
+
+
+class VenueAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'town_or_city',
+        'postcode',
+    )
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'team_name',
+        'age',
+        'manager_coach',
+    )
+
+
+class FeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'age',
+        'referee_fee',
+        'asst_referee',        
+    )
+
+    ordering = ('age',)
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        'home_team',
+        'away_team',
+        'venue',
+        'date_time',
+    )
+
+    ordering = ('date_time',)
+
+
 admin.site.register(Age_Group)
-admin.site.register(Club)
-admin.site.register(Venue)
-admin.site.register(Team)
-admin.site.register(Game)
-admin.site.register(Fee)
+admin.site.register(Club, ClubAdmin)
+admin.site.register(Venue, VenueAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Fee, FeeAdmin)
