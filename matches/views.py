0c27ec6db5f720daec9452b830cd49f1d47b16aa
GazzaJ <johnge71@gmail.com>
2021-05-28 15:21:06 +0000

    Added Matches views and templates

--- matches/views.py
+++ matches/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Venue, Club, Age_Group, Fee, Team, Game
 
-# Create your views here.
+
+def all_matches(request):
+    """
+    This view will render the matches
+    including, sorting and searching
+    """
+
+    matches = Game.object.all()
+
+    context = {
+        'matches': matches,
+    }
+
+    return render(request, 'matches/matches.html', context)
