f9a6991b529a24b691495a1975df9884243d6e13
GazzaJ <johnge71@gmail.com>
2021-06-14 09:12:34 +0000

    Provide add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -9,5 +9,17 @@ def review_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, match_id):
-    """ Add the match fees tto the shopping bag """
+def add_to_bag(request, item_id):
+    """ Add the match fees to the shopping bag """
+    quantity = int(request.POST.get('ref-fee'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
