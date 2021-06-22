4ebed28756b985b4e6171309160b980137377f10
GazzaJ <johnge71@gmail.com>
2021-06-22 13:21:19 +0000

    update bag to take the ref and asst ref total & calc the match and grand total

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,23 +6,35 @@ from matches.models import Game
 def bag_contents(request):
 
     bag_items = []
+    ref_total = 0
+    asst1_total = 0
+    asst2_total = 0
     grand_total = 0
 
-    bag = request.session.get('bag', {})
+    bag = request.session.get('bag', {})      
 
     for item_id, quantity in bag.items():
         match = get_object_or_404(Game, pk=item_id)
+        match_total = match.ref_total + match.asst1_total + match.asst2_total
+
+        grand_total += match_total
 
         bag_items.append({
             'item_id': item_id,
+            'ref_total': ref_total,
+            'asst1_total': asst1_total,
+            'asst2_total': asst2_total,
+            'match_total': match_total,
             'grand_total': grand_total,
             'match': match,
+
             'quantity': quantity,
-        })   
+        })
 
     context = {        
         'bag_items': bag_items,
         'grand_total': grand_total,
+        'ref_total': ref_total,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
+@login_required
 def review_bag(request):
     """ This view will render the contents of our bag
     e.g. the matches we are trying to pay for
     """
-
+    
     return render(request, 'bag/bag.html')
 
 
+@login_required
 def add_to_bag(request, item_id):
-    """ Add the match fees to the shopping bag """
+    """ Add the match fees to the shopping bag """    
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get('redirect_url')    
 
     # Check if bag in session, create one if not
     bag = request.session.get('bag', {})
