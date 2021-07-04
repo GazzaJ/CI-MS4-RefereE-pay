87b32b19d1a8b02ed06905c10d56d062a99962bd
GazzaJ <johnge71@gmail.com>
2021-07-04 11:10:44 +0000

    Added webhooks, created webhook url and added Stripe WH Key

--- referee_pay/settings.py
+++ referee_pay/settings.py
@@ -191,6 +191,7 @@ NONPAYMENT_FINE = 25
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY_MS4', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY_MS4', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET_MS4', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
