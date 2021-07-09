38338753a4317d4f8d3c8de277831dfa1fb30969
GazzaJ <johnge71@gmail.com>
2021-07-09 10:45:50 +0000

    Update webhook handler to use profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from matches.models import Game
+from profiles.models import UserProfile
 
 import json
 import time
@@ -32,6 +33,20 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         grand_total = round(intent.charges.data[0].amount/100, 2)
+
+        # Update profile information if save_info checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.profile_phone_number = billing_details.phone
+                profile.profile_street_address1 = billing_details.address.line1,
+                profile.profile_street_address2 = billing_details.address.line2,
+                profile.profile_town_or_city = billing_details.address.city,
+                profile.profile_county = billing_details.address.state,
+                profile.profile_country = billing_details.address.country,
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -64,6 +79,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=billing_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=billing_details.phone,
                     street_address1=billing_details.address.line1,
