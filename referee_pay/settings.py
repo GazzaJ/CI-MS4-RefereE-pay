5739b5212ea751b9534c9bc854ef4c602962dd54
GazzaJ <johnge71@gmail.com>
2021-07-23 13:51:52 +0000

    Removed Django secret key from settings and set debug for development

--- referee_pay/settings.py
+++ referee_pay/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'emj%dx)j-+4m%#(xg+s6=29_tj8hm7+)wapeef@aejq+c4%e3!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ci-ms4-referee-pay.herokuapp.com', 'localhost']
 
