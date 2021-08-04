6fa4038a9765b1240ef503a1ade68252f55a0747
GazzaJ <johnge71@gmail.com>
2021-08-04 15:37:42 +0000

    Add Profile View test

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,11 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import User, UserProfile
+from .forms import UserProfileForm
 
 
-class TestProfileViews(TestCase):
+class TestProfileViews(TestCase):    
 
     def test_get_profile_page(self):
-        response = self.client.get('/profile')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'profiles/profile.html')
+        user = User.objects.create_user(
+            'testuser',
+            'test@email.com',
+            'testpassword'
+        )
 
-    # def test_edit_profile(self):
+        self.client.login(
+            username='testuser',
+            password='testpassword'
+        )
+
+        response = self.client.get("/profile/")
+        self.assertEqual(response.status_code, 200)        
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+        self.assertTemplateUsed(response, 'base.html')
