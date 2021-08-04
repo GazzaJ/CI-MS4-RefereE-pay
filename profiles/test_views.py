c3954a647c699165816a99d6232ed6c7fea8f8af
GazzaJ <johnge71@gmail.com>
2021-08-04 10:11:54 +0000

    Begin testing views - Profile views

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,4 +1,11 @@
 from django.test import TestCase
 
 
+class TestProfileViews(TestCase):
 
+    def test_get_profile_page(self):
+        response = self.client.get('/profile')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+
+    # def test_edit_profile(self):
