4f4b16f85798651956d48839080ae47633194a68
GazzaJ <johnge71@gmail.com>
2021-08-27 12:44:48 +0000

    Refactor code for PEP8 compliance

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,22 +4,23 @@ from matches.models import Game
 
 import datetime
 
+
 def bag_contents(request):
 
-    bag_items = []    
+    bag_items = []
     match_total = 0
     match_sub_total = 0
     grand_total = 0
     match_count = 0
-    match_fines = 0    
+    match_fines = 0
 
-    bag = request.session.get('bag', {})      
+    bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
         match = get_object_or_404(Game, pk=item_id)
         match_sub_total = match.ref_total + match.asst1_total + match.asst2_total
         payment_due_date = match.date_time + datetime.timedelta(days=1)
-        today = datetime.datetime.now()        
+        today = datetime.datetime.now()
         if today > payment_due_date.replace(tzinfo=None):
             match_fines = settings.NONPAYMENT_FINE
         else:
@@ -39,13 +40,13 @@ def bag_contents(request):
             'match_sub_total': match_sub_total,
         })
 
-    context = {        
+    context = {
         'bag_items': bag_items,
         'match_total': match_total,
         'grand_total': grand_total,
         'match_count': match_count,
-        'match_sub_total': match_sub_total,        
-        'match_fines': match_fines,    
+        'match_sub_total': match_sub_total,
+        'match_fines': match_fines,
     }
 
     return context
