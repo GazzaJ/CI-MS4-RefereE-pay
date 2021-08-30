5e9d0fbd315f0ac117a7446b07f2870e6a57d49c
GazzaJ <johnge71@gmail.com>
2021-08-30 11:05:06 +0000

    Correct Flake 8 errors in checkout webhooks and webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -132,7 +132,7 @@ class StripeWH_Handler:
                  Created order in webhook',
             status=200)
 
-    def handle_payment_intent_payment_failed(self, event):
+    def handle_payment_intent_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
