6bc70525137da581662d7d159739c47e0f613858
GazzaJ <johnge71@gmail.com>
2021-08-30 08:56:46 +0000

    Validate JS in Profile app

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,7 +1,7 @@
 let countrySelected = $('#id_profile_country').val();
 if(!countrySelected) {
     $('#id_profile_country').css('color', '#aab7c4');
-};
+}
 $('#id_profile_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
